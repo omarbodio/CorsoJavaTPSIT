c303fed3a1d2fdf554e9a0085d39838006d87298
Omar Bodio <omar.bodio@marconi.loc>
2021-02-05 08:42:32 +0100

    first commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+class Runner extend Thread {
+    @Override
+    public void run() {
+        for(int i=0; i<10; i++){
+            System.out.println("Hello "+1);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException) {
+                //TODO: handle exception
+                e.printStackTrace();
+            }
+        }
     }
 }
