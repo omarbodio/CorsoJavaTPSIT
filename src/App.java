ed82e672b5d468722fe2a75a830c477c54b7cc28
Omar Bodio <omar.bodio@marconi.loc>
2021-02-05 08:59:41 +0100

    Add a new Runner (runner2)

--- src/App.java
+++ src/App.java
@@ -1,12 +1,12 @@
-class Runner extend Thread {
+class Runner extends Thread {
     @Override
     public void run() {
         for(int i=0; i<10; i++){
-            System.out.println("Hello "+1);
+            System.out.println("Hello "+ i);
 
             try {
                 Thread.sleep(100);
-            } catch (InterruptedException) {
+            } catch (InterruptedException e) {
                 //TODO: handle exception
                 e.printStackTrace();
             }
@@ -16,7 +16,10 @@ class Runner extend Thread {
 
 public class App{
     public static void main(String[] args) throws Exception {
-        Runner runner = new Runner();
-        runner.start();
+        Runner runner1 = new Runner();
+        runner1.start();
+
+        Runner runner2 = new Runner();
+        runner2.start();
     }
 }
