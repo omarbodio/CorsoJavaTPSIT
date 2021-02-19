bd4447e4a2821d1f279fc3b00f87c818ea66592c
Omar Bodio <omar.bodio@marconi.loc>
2021-02-19 09:08:33 +0100

    add class Processor

--- src/App.java
+++ src/App.java
@@ -1,3 +1,19 @@
+public class Processor extends Thread{
+    private boolean running = true;
+    @Override
+    public void run() {
+        while(running)
+        {
+            System.out.println("hello");
+            try {
+                Thread.sleep(100);
+            } catch (Exception e) {
+                //TODO: handle exception
+                e.printStackTrace();
+            }
+        }
+    }
+}
 
 public class App {
 
