8fa4d16f93b62dac0fcee9af3687639e5b67c23f
Omar Bodio <omar.bodio@marconi.loc>
2021-02-19 09:25:53 +0100

    do Processor.java file

--- src/App.java
+++ src/App.java
@@ -1,25 +1,6 @@
-import java.util.Scanner;
+import java.util.*;
+import System.*;
 
-
-public class Processor extends Thread{
-    private boolean running = true;
-    @Override
-    public void run() {
-        while(running)
-        {
-            System.out.println("hello");
-            try {
-                Thread.sleep(100);
-            } catch (Exception e) {
-                //TODO: handle exception
-                e.printStackTrace();
-            }
-        }
-    }
-    public void shutdown() {
-        running = false;
-    }
-}
 
 public class App {
 
