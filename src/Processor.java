c5077b976e1d4b019169efadbdfc39370d039b3f
omar.bodio <omar.bodio@marconirovereto.it>
2021-03-26 08:37:22 +0100

    first commit 26 03 2021

--- src/Processor.java
+++ src/Processor.java
@@ -1,41 +1,29 @@
-class Processor extends Runnable {
-   
-    private int id;
+import java.util.concurrent.CountDownLatch;
 
-    public Processor(int id) {
-        this.id = id;
-    }
+class Processor implements Runnable{
+    private CountDownLatch latch;
+     
+
     public void run() {
-        System.out.println("Starting: " + id);
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-        }
-        System.out.println("Complete:" + id);
-    }
-    
-}
-public class App {
-    import java.util.concurrent.TimeUnit;
-    import java.util.concurrent.Executors;
-    import java.util.concurrent.ExecutorService;
-    
-    
-    public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(2);
-        for (int i = 0; i < 5; i++) {
-            executor.submit(new Processor(i));
-        }
-        executor.shutdown();
-
-        System.out.println("All tasks sumitted.");
-
-        try {
-            executor.awaitTermination(1, TimeUnit.DAYS);
-        } catch (InterruptedException e){
-        }
-
-        System.out.println("All task complted.");
-    }
+        System.out.println("Started.");
         
+        try {
+            Thread.sleep(3000);
+        } catch (Exception e) {
+        //TODO: handle exception
+            e.printStackTrace();
+        }
+
+        latch.countDown();
+
+    }
 }
+class App extends {
+    public static void main(String[] args) {
+        
+        CountDownLatch latch = new CountDownLatch(3);
+        
+    }
+}
+
+
